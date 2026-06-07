069a2c2aede16d95637b05f2058d5ac72cc10000
aaroncmdo <aaron.sprafke@claimondo.de>
2026-06-07 21:00:46 +0200

    fix(cluster-lp): Wuppertal + Bonn auf GTM-KD2L63T3 (Duesseldorf bleibt GTM-KZNCZB2Z)

--- kfz-gutachter-bonn/app/layout.tsx
+++ kfz-gutachter-bonn/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KZNCZB2Z"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KD2L63T3"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
@@ -48,7 +48,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         </noscript>
         {/* Google Tag Manager */}
         <Script id="gtm-base" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KZNCZB2Z');`}
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KD2L63T3');`}
         </Script>
         {children}
         <CookieConsentBanner />
