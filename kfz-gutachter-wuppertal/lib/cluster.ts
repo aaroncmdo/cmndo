edef00325d693578a08a56227d66f445c14645be
aaroncmdo <aaron.sprafke@claimondo.de>
2026-06-11 22:07:40 +0200

    chore(cluster-wuppertal): Persona-Nachname Egetuerk (Amet Egetuerk)
    svSurname 'Kraemer' -> 'Egetürk' (Aaron-Vorgabe). Voller Name jetzt "Amet Egetürk".

--- kfz-gutachter-wuppertal/lib/cluster.ts
+++ kfz-gutachter-wuppertal/lib/cluster.ts
@@ -91,7 +91,7 @@ export const CLUSTER: ClusterConfig = {
   h1SubSpan: 'Unabhängige Sachverständige. Gerichtsfeste Gutachten nach DAT-Standard.',
   teamImg: '/assets/img/wuppertal/team-wuppertal.webp?v=1',
   svName: 'Amet',
-  svSurname: 'Krämer', // Persona-Nachname (Amet Krämer)
+  svSurname: 'Egetürk', // Persona-Nachname (Amet Egetürk)
   phone: { display: '+49 1515 3608515', displayNational: '0151 5360 8515', tel: '+4915153608515', wa: '4915153608515' },
   landmark: { label: 'Schwebebahn', img: 'stadt-wuppertal.png' },
   facts: [
